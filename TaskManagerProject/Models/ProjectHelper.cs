e00bf5938fdc8a0fe6d6c4ee57813257b33e3720
Antony Solis <antonysolis@live.ca>
2020-05-28 10:57:25 -0500

    Add ProjectHelper class

---
+++ TaskManagerProject/Models/ProjectHelper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagerProject.Models
+{
+    public class ProjectHelper
+    {
+        public virtual Project Project { get; set; };
+
+        static ApplicationDbContext db = new ApplicationDbContext();
+
+        static UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>
+            (new UserStore<ApplicationUser>(db));
+
+        static RoleManager<IdentityRole> projectManager = new RoleManager<IdentityRole>
+            (new RoleStore<IdentityRole>(db));
+
+        public static List<string> GetAllProjects()
+        {
+            return ;
+        }
+
+        public static void CreateProject(string projectName)
+        {
+            if (!projectManager.RoleExists(projectName))
+            {
+                projectManager.Create(new Project { Name = projectName });
+            }
+        }
+        public static void DeleteProject(string projectName)
+        {
+            if (projectManager.RoleExists(projectName))
+            {
+                projectManager.Delete(new Project { Name = projectName });
+            }
+        }
+        public static List<string> ProjectsForUser(string userId)
+        {
+            return userManager.GetRoles(userId).ToList();
+        }
+    }
+}
