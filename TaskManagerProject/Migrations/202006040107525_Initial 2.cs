03d19a4c6b6f302a58a9cb3e62d37e9f48a040c7
jess <jesdl604@hotmail.com>
2020-06-03 20:14:34 -0500

    Add-Migration "Initial 2", Update-Database

---
+++ TaskManagerProject/Migrations/202006040107525_Initial 2.cs
@@ -0,0 +1,22 @@
+﻿namespace TaskManagerProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial2 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Notes", "ProjectId", "dbo.Projects");
+            DropIndex("dbo.Notes", new[] { "ProjectId" });
+            DropColumn("dbo.Notes", "ProjectId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Notes", "ProjectId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Notes", "ProjectId");
+            AddForeignKey("dbo.Notes", "ProjectId", "dbo.Projects", "Id", cascadeDelete: true);
+        }
+    }
+}
