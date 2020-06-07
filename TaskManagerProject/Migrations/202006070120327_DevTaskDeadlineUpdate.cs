c2d7185b3669038c382957807e894c6856ebb0ed
Antony Solis <antonysolis@live.ca>
2020-06-06 20:23:38 -0500

    Commented out code in configuration

---
+++ TaskManagerProject/Migrations/202006070120327_DevTaskDeadlineUpdate.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskManagerProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DevTaskDeadlineUpdate : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.DevTasks", "Deadline", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.DevTasks", "Deadline", c => c.DateTime(nullable: false));
+        }
+    }
+}
